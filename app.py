d674b01980c73672397d381728db96f212396692
2eliot <yorbitub@gmail.com>
2026-05-06 18:22:03 -0400

    Bust stale public page cache

--- app.py
+++ app.py
@@ -91,6 +91,10 @@ def _disable_admin_cache(response):
             response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
             response.headers["Pragma"] = "no-cache"
             response.headers["Expires"] = "0"
+        elif response.mimetype == "text/html":
+            response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+            response.headers["Pragma"] = "no-cache"
+            response.headers["Expires"] = "0"
     except Exception:
         pass
     return response
